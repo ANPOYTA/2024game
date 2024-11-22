409e4509c822ad1276e5ab56eb43f5056067ea6c
Anna Poyta <149379470+ANPOYTA@users.noreply.github.com>
2024-11-22 13:27:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ on(window, 'load', function () {
     game.init(view);
     event(game);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const intro = document.querySelector('.intro-animation');
+    setTimeout(() => {
+        intro.style.opacity = '0';
+        setTimeout(() => {
+            intro.style.display = 'none';
+        }, 1000);
+    }, 2000);
+});
